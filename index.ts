2ee7150bf36cf926d6f2aecaff41e92df13e2ac8
Haider Mehdi <haidershah659@gmail.com>
2023-10-18 18:39:07 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,33 @@
+let guestList = [
+    'Asad Raza',
+    'Zamaan Khan',
+    'Daniyal Qaiser',
+    'Rehan Asif',
+    'Umair Gujjar',
+    'Huzaifa Gujjar'
+  ]
+ 
+// Step 1
+  console.log(`I can invite only two people for dinner.`)
+
+//   Step 2
+  while (guestList.length > 2){
+    let removeGuest = guestList.pop()
+    console.log (`Sorry ${removeGuest}, Due to some reason I can't invite you to dinner.`)
+
+  }
+
+//   Step 3
+guestList.forEach((guest) => {
+    console.log (`Hi ${guest}, you're still invited to dinner.`)    
+});
+
+// Step 4
+// guestList.forEach((guest) => {
+    
+    guestList.pop()
+    guestList.pop()
+
+
+// Step 5
+console.log(`Guest List after dinner:`, guestList )
